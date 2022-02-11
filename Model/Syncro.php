b4a419a555ec65a021c8664f430592ae298b1cbe
Salvatore Pruiti <salvatorepruiti@hotmail.com>
2022-02-11 16:04:04 +0100

    Nuova Sincronizzazione tramite Oggetto JSON

--- Model/Syncro.php
+++ Model/Syncro.php
@@ -2,6 +2,11 @@
 
 namespace ScaliaGroup\Integration\Model;
 
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Webapi\Rest\Request;
 use ScaliaGroup\Integration\Api\SyncroInterface;
 
@@ -9,10 +14,15 @@ class Syncro implements SyncroInterface
 {
 
     private $request;
+    protected $productFactory;
+    protected $productRepository;
 
     public function __construct(Request $request)
     {
         $this->request = $request;
+        $this->productFactory = ObjectManager::getInstance()->create(ProductFactory::class);
+        $this->productCollection = ObjectManager::getInstance()->create(Collection::class);
+        $this->productRepository = ObjectManager::getInstance()->create(ProductRepository::class);
     }
 
     public function post()
@@ -21,7 +31,41 @@ class Syncro implements SyncroInterface
 
         $products = $body['products'] ?? [];
 
-        echo json_encode($products, 128);
+        foreach($products as $product) {
+
+            foreach($product['variations'] as $simpleProduct) {
+
+                try {
+
+
+                    echo $simpleProduct['sku'] . "\n";
+
+                    /** @var Product $product */
+                    $product = $this->productFactory->create()->loadByAttribute('sku', $sku = $simpleProduct['sku']);
+                    if(!$product) {
+                        $product = $this->productFactory->create();
+
+                        $product->setSku($sku);
+                    }
+                    $product->setName($product['name']);
+                    echo "Magento SKU: " . $product->getSku() . "\n";
+                    echo "StoreID: " . $product->getStoreId() . "\n";
+
+
+
+                } catch (\Throwable $e) {
+                    echo $e->getMessage() ."\n";
+                    exit;
+                }
+
+
+            }
+
+
+
+
+        }
+
 
 
 
